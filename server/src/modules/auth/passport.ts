dfdefd008991d982f5dc2c7a27e80283884f55e4
Jong-ku Lee <addy94@naver.com>
2020-08-12 16:21:04 +0900

    feat: backend boilerplate 작성 #18 - mvc 디렉토리 구조 작성 - express 기본 설정 - custom logger 설정 - custom error handler 설정 - passport jwt, google 전략 설정 - apidoc 설정 - nodemon 설정 - jest config 작성 - dotenv 설정 - sequelize 데이터베이스 제외 보일러플레이트 코드 작성했습니다.

---
+++ server/src/modules/auth/passport.ts
@@ -0,0 +1,50 @@
+import passport from 'passport';
+import { Strategy as JwtStrategy } from 'passport-jwt';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import logger from '../../config/logger';
+import { Request } from 'express';
+import { jwtSecret, googleCredentials } from '../../config/consts';
+
+const jwtFromRequest = (req: Request) => {
+  let token = null;
+  if (req && req.headers.authorization) {
+    token = req.headers.authorization;
+    token = token.replace('Basic ', '');
+  }
+  return token;
+};
+const secretOrKey = jwtSecret;
+const opts = { jwtFromRequest, secretOrKey };
+
+const jwt = new JwtStrategy(opts, (jwt_payload: any, done) => {
+  logger.info('JWT BASED AUTH GETTING CALLED');
+  if (jwt_payload.data) {
+    // check user valid
+    return done(null, jwt_payload.data);
+  } else {
+    return done(null, false);
+  }
+});
+
+const google = new GoogleStrategy(
+  {
+    clientID: googleCredentials.clientId,
+    clientSecret: googleCredentials.clientSecret,
+    callbackURL: googleCredentials.callback,
+  },
+  (accessToken, refreshToken, profile, done: Function) => {
+    console.info('GOOGLE BASED OAUTH VALIDATION GETTING CALLED');
+    return done(null, profile);
+  }
+);
+
+passport.serializeUser((user, done) => {
+  logger.debug('serializeUser');
+  done(null, user);
+});
+passport.deserializeUser((obj, done) => {
+  logger.debug('deserializeUser');
+  done(null, obj);
+});
+
+export default { jwt, google };
