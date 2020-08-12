265bd34eb646ae681a7200f9bb86a497c1a9f29a
younho9 <younho9.choo@gmail.com>
2020-08-12 17:17:26 +0900

    test: Header 컴포넌트 테스트 양식 작성 #33

---
+++ client/src/components/modules/Header/Header.spec.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent } from '@testing-library/react';
+
+import { Header } from './Header';
+
+describe('<Header />', () => {
+  it('matches snapshot', () => {
+    const { container } = render(<Header />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('shows the elemnts correctly', () => {
+    const utils = render(<Header />);
+  });
+});
