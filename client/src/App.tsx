a365b14642a2bd4ba6f87be14bb6e2f31960c514
Jong-ku Lee <addy94@naver.com>
2020-08-12 16:20:06 +0900

    chore: prettier 미적용 코드 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={ MainPage }/>
+        <Route exact path="/" component={MainPage} />
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
