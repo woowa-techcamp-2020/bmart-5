99c6190d842f10052b5f41c59a4b5419cfae104b
younho9 <younho9.choo@gmail.com>
2020-08-13 00:24:35 +0900

    style: 브라우저 스타일을 초기화 하는 Normalize 추가 #33
    - 브라우저 스타일을 초기화하기 위해 Normalize를 GlobalStyle로 사용했습니다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import MainPage from '@pages/MainPage';
+import Normalize from '@commons/styles/Normalize';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={MainPage} />
-      </Switch>
-    </div>
+    <React.Fragment>
+      <Normalize />
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+        </Switch>
+      </div>
+    </React.Fragment>
   );
 };
 
