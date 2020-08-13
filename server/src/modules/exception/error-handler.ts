2495a5bf797996374c5d574349389fbd96d7d118
Jong-ku Lee <addy94@naver.com>
2020-08-13 16:22:59 +0900

    refactor: error 응답에 error.stack 추가

--- server/src/modules/exception/error-handler.ts
+++ server/src/modules/exception/error-handler.ts
@@ -2,20 +2,13 @@ import { Request, Response, NextFunction } from 'express';
 import CustomError from './custom-error';
 import HttpStatus from 'http-status';
 import logger from '../../config/logger';
+import { JsonResponse } from '../utils';
 
-const errorHandler = (
-  error: CustomError,
-  request: Request,
-  response: Response,
-  next: NextFunction
-) => {
+const errorHandler = (error: CustomError, req: Request, res: Response, next: NextFunction) => {
   const status = error.status || HttpStatus.INTERNAL_SERVER_ERROR;
   const message = error.message || 'Something went wrong';
   logger.error(error.stack);
-  response.status(status).send({
-    status,
-    message,
-  });
+  res.status(status).json(JsonResponse(status, message, { error: error.stack }));
 };
 
 export default errorHandler;
