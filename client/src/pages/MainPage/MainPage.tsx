c3611603834500522035c33c931616218cc9f877
younho9 <younho9.choo@gmail.com>
2020-08-13 19:40:31 +0900

    chore: Header 컴포넌트 예시 MainPage에 추가 #64

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import Header from '@components/modules/Header';
 import SlidableContainer from '@components/modules/SlidableContainer';
+import { IconType } from '@utils/constants';
 
 const MainPage: React.FC = () => {
   return (
     <div>
-      <Header />
+      {/* main */}
+      <Header
+        left={IconType.ARROW_LEFT}
+        main="Logo"
+        right={[IconType.SEARCH, IconType.BARS]}
+      />{' '}
+      {/* back & title */}
+      <Header left={IconType.ARROW_LEFT} main="장바구니" />
+      {/* back */}
+      <Header left={IconType.ARROW_LEFT} />
+      {/* back & title */}
+      <Header left={IconType.CLOSE} main="이벤트" />
+      {/* category */}
+      <Header
+        left={IconType.ARROW_LEFT}
+        main="정육,수산,계란"
+        right={[IconType.SEARCH, IconType.BARS]}
+      />{' '}
+      {/* TODO: SearchBar */}
+      <Header left={IconType.ARROW_LEFT} main="SearchBar" right={[IconType.SEARCH]} />{' '}
+      {/* OrderList */}
+      <Header left={IconType.NOTIFICATION} main="주문내역" right={[IconType.REFRESH]} />{' '}
       <SlidableContainer />
     </div>
   );
