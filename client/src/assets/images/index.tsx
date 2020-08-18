1dd2839f39deba3ccf2c7c97c9c7391e6601255c
younho9 <younho9.choo@gmail.com>
2020-08-19 00:45:20 +0900

    chore: 클라이언트 asset 폴더의 이미지 가져오기 #93

---
+++ client/src/assets/images/index.tsx
@@ -0,0 +1,11 @@
+export { default as Coke } from './coke.jpeg';
+export { default as MainBread } from './main-bread.png';
+export { default as MainChoco } from './main-choco.png';
+export { default as MainEgg } from './main-egg.png';
+export { default as MainHair } from './main-hair.png';
+export { default as MainIce } from './main-ice.png';
+export { default as MainMealkit } from './main-mealkit.png';
+export { default as MainMilk } from './main-milk.png';
+export { default as MainSalad } from './main-salad.png';
+export { default as MainYasik } from './main-yasik.png';
+export { default as More } from './more.png';
