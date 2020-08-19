88f9f257298ce4efac12a4691d34b21bcd18d50e
younho9 <younho9.choo@gmail.com>
2020-08-19 21:26:33 +0900

    chore: 문구 수정 #69

--- client/src/components/templates/TabViewContainer/TabViewContainer.tsx
+++ client/src/components/templates/TabViewContainer/TabViewContainer.tsx
@@ -7,7 +7,7 @@ type Props = {};
 export const TabViewContainer: React.FC<Props> = (props) => {
   return (
     <S.TabViewContainer>
-      <ContainerHeader moreBtn>지금사면 ⚡️ 번쩍할인</ContainerHeader>
+      <ContainerHeader moreBtn>지금사면 ⚡️번쩍할인</ContainerHeader>
     </S.TabViewContainer>
   );
 };
