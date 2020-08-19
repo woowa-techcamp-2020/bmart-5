fb7e09d942681175f6071bf682e1156a525415d1
younho9 <younho9.choo@gmail.com>
2020-08-19 15:46:21 +0900

    feat: ContainerHeader 오른쪽 더보기 버튼 추가 가능하도록 개선 #69

--- client/src/components/modules/ContainerHeader/ContainerHeader.tsx
+++ client/src/components/modules/ContainerHeader/ContainerHeader.tsx
@@ -1,10 +1,17 @@
 import React, { ReactNode } from 'react';
 import * as S from './styled';
+import SubButton from '@components/atoms/SubButton';
 
 type Props = {
-  children: ReactNode;
+  children?: ReactNode;
+  moreBtn?: boolean;
 };
 
-export const ContainerHeader: React.FC<Props> = ({ children }) => {
-  return <S.ContainerHeader>{children}</S.ContainerHeader>;
+export const ContainerHeader: React.FC<Props> = ({ children, moreBtn }) => {
+  return (
+    <S.ContainerHeader>
+      {children}
+      {moreBtn && <SubButton withArrow color="#2CC0BD" label="더보기" />}
+    </S.ContainerHeader>
+  );
 };
