366a81ed983f49d675919efdbc489e6e622b2bdc
younho9 <younho9.choo@gmail.com>
2020-08-19 15:47:19 +0900

    feat: TabViewContainer에 Header 추가 #69

--- client/src/components/templates/TabViewContainer/TabViewContainer.tsx
+++ client/src/components/templates/TabViewContainer/TabViewContainer.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import * as S from './styled';
+import ContainerHeader from '@components/modules/ContainerHeader';
 
 type Props = {};
 
 export const TabViewContainer: React.FC<Props> = (props) => {
-  return <S.TabViewContainer />;
+  return (
+    <S.TabViewContainer>
+      <ContainerHeader moreBtn>지금사면 ⚡️ 번쩍할인</ContainerHeader>
+    </S.TabViewContainer>
+  );
 };
-
