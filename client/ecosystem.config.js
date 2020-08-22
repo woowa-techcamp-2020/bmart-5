77ed9ef6f775770d4eda88b7144bdabe1fb9577d
Jong-ku Lee <addy94@naver.com>
2020-08-22 17:24:25 +0900

    fix: 실행 script, pm2 실행 config 오류 수정 #115 - root install:all 스크립트에 yarn install 포함 - 각 pm2 watch-ignoredp logs 폴더 추가

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -5,7 +5,15 @@ module.exports = {
       script:
         './node_modules/next/dist/bin/next build && ./node_modules/next/dist/bin/next start --port 80',
       watch: true,
-      ignore_watch: ['node_modules', '.vscode', '.github', 'scripts', '.next', '.storybook'],
+      ignore_watch: [
+        'logs',
+        'node_modules',
+        '.vscode',
+        '.github',
+        'scripts',
+        '.next',
+        '.storybook',
+      ],
       out_file: './logs/out.log',
       error_file: './logs/error.log',
     },
