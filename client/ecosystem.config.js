5084543976d187e995aa2ba626b4ca3d164994ac
Jong-ku Lee <addy94@naver.com>
2020-08-23 02:07:14 +0900

    fix: remove watch option for pm2

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -3,16 +3,7 @@ module.exports = {
     {
       name: 'bmart-web',
       script: './node_modules/next/dist/bin/next start --port 80',
-      watch: true,
-      ignore_watch: [
-        'logs',
-        'node_modules',
-        '.vscode',
-        '.github',
-        'scripts',
-        '.next',
-        '.storybook',
-      ],
+      watch: false,
       out_file: './logs/out.log',
       error_file: './logs/error.log',
     },
--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,16 +3,7 @@ module.exports = {
     {
       name: 'bmart-api',
       script: 'node ./node_modules/ts-node/dist/bin.js -r tsconfig-paths/register ./src/main.ts',
-      watch: true,
-      ignore_watch: [
-        'logs',
-        'node_modules',
-        '.vscode',
-        '.github',
-        'scripts',
-        '.next',
-        '.storybook',
-      ],
+      watch: false,
       out_file: './logs/out.log',
       error_file: './logs/error.log',
     },
