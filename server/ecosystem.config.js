81151935238c2d4fdf51151e8bcbaffd0dd2b54c
Jong-ku Lee <addy94@naver.com>
2020-08-23 10:16:24 +0900

    chore: remove pm2 logs from server

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -4,8 +4,6 @@ module.exports = {
       name: 'bmart-api',
       script: 'node ./node_modules/ts-node/dist/bin.js -r tsconfig-paths/register ./src/main.ts',
       watch: false,
-      out_file: './logs/out.log',
-      error_file: './logs/error.log',
     },
   ],
 };
