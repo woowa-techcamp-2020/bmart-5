1bd624d11e6e8af977b98fb9548ecbb43b5c09e5
younho9 <younho9.choo@gmail.com>
2020-08-26 22:43:20 +0900

    feat: Product 데이터 파일 목록에서 bulk create 하도록 수정 #126

--- server/src/bulk-data/index.ts
+++ server/src/bulk-data/index.ts
@@ -3,11 +3,7 @@ import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
 import { Category, SubCategory, Product, User, LoginProvider, Cart } from '../models';
 import { jwtSecret } from '@config/constants';
-import REGEX from '@shared/validate';
 import logger from '@config/logger';
-import CustomError from 'src/modules/exception/custom-error';
-import httpStatus from 'http-status';
-
 const readData = (file: string) => {
   return JSON.parse(fs.readFileSync(`${__dirname}/${file}`, { encoding: 'utf-8' }));
 };
@@ -21,47 +17,33 @@ const createAdminUser = async (body: {
   email: string;
   password: string;
 }): Promise<string> => {
-  try {
-    if (!REGEX.PW_REGEX.test(body.password))
-      throw new CustomError(
-        httpStatus.BAD_REQUEST,
-        `Validation Error: Password`,
-        'Validation Error: Password'
-      );
-    body.password = crypto.createHash('sha256').update(body.password).digest('base64');
-    const user = await User.create({
-      username: body.username,
-      email: body.email,
-      isAdmin: true,
-    });
-    await Cart.create({ userId: user.id });
-    const emailUserData = {
-      id: user.id.toString(),
-      email: body.email,
-      password: body.password,
-      provider: 'email',
-      userId: user.id,
-    };
-    await LoginProvider.create(emailUserData);
-
-    return jwt.sign(
-      {
-        data: user,
-      },
-      jwtSecret,
-      { expiresIn: '1d' }
-    );
-  } catch (err) {
-    logger.error('admin created fail', err);
-    return '';
-  }
+  body.password = crypto.createHash('sha256').update(body.password).digest('base64');
+  const user = await User.create({
+    username: body.username,
+    email: body.email,
+    isAdmin: true,
+  });
+  Cart.create({ userId: user.id });
+  const emailUserData = {
+    id: user.id.toString(),
+    email: body.email,
+    password: body.password,
+    provider: 'email',
+    userId: user.id,
+  };
+  LoginProvider.create(emailUserData);
+  return jwt.sign(
+    {
+      data: user,
+    },
+    jwtSecret,
+    { expiresIn: '1d' }
+  );
 };
-
 const bulkData = async () => {
   // create Categories
   const categoryData = readData('data/category.json');
   await Category.bulkCreate(categoryData);
-
   // create SubCategories
   const subCategoryData = readData('data/sub-category.json');
   await SubCategory.bulkCreate(subCategoryData);
@@ -77,5 +59,4 @@ const bulkData = async () => {
   const token = await createAdminUser(signupAdminUser);
   logger.info(`admin token: ${token}`);
 };
-
 export default bulkData;
