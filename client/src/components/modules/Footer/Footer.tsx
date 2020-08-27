3615e58be06ab91f8f91c587c3a33ec926e1d91c
MyeongSeong <maeng2418@naver.com>
2020-08-27 15:00:08 +0900

    feat: footer 구현#158

--- client/src/components/modules/Footer/Footer.tsx
+++ client/src/components/modules/Footer/Footer.tsx
@@ -1,8 +1,19 @@
 import * as React from 'react';
 import * as S from './styled';
+import { CALL_NUMBER, SERVICE_EMAIL } from '@utils/constants';
 
 export const Footer = () => (
   <S.Footer>
-    <div>footer</div>
+    <S.ButtonContainer>
+      <S.Button>반품·교환하기</S.Button>
+    </S.ButtonContainer>
+    <S.FooterContent>
+      <S.Row>
+        <span>고객센터 오전 9시 ~ 새벽 3시</span> | <span>{CALL_NUMBER}</span>
+      </S.Row>
+      <S.Row>
+        <span>제휴문의</span> | <span>{SERVICE_EMAIL}</span>
+      </S.Row>
+    </S.FooterContent>
   </S.Footer>
 );
--- client/src/utils/constants.tsx
+++ client/src/utils/constants.tsx
@@ -103,3 +103,7 @@ export const MaxSubCategoryCount = 132;
 export const MaxProductsCountByMainCategoryContainer = 10;
 
 export const MaxProductsCountByCategoryPageContainer = 100;
+
+export const CALL_NUMBER = '1600-0025';
+
+export const SERVICE_EMAIL = 'bmart_together@woowahan.com';
