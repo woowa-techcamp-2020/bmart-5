d76f085786175c67b8f4f851d95682fa81d77c3f
younho9 <younho9.choo@gmail.com>
2020-08-27 19:29:40 +0900

    feat: FetchableContainer 추가 #164

--- client/src/components/templates/FetchableContainer/FetchableContainer.tsx
+++ client/src/components/templates/FetchableContainer/FetchableContainer.tsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import * as S from './styled';
+import Icon from '@components/atoms/Icon';
+import ProductCard from '@components/modules/ProductCard';
+import { ProductType } from '@pages/index';
+import ContainerHeader from '@components/modules/ContainerHeader';
+import { Context } from '@commons/Context';
+import { IconType } from '@utils/constants';
 
-type Props = {};
+type ProductArrType = Array<ProductType>;
 
-export const FetchableContainer: React.FC<Props> = (props) => {
-  return <S.FetchableContainer />;
+type Props = {
+  title: string;
+  products: ProductArrType;
 };
 
+export const FetchableContainer: React.FC<Props> = ({ title, products }) => {
+  const { likeProducts, setLikeProducts } = useContext(Context);
+  const totalSlides = Math.floor(products.length / 6);
+  const [currentSlide, setCurrentSlide] = useState(1);
+
+  return (
+    <S.FetchableContainer>
+      <ContainerHeader>{title}</ContainerHeader>
+      <div className="wrapper">
+        <div className="content">
+          {products.map((item: ProductType, idx: number) => {
+            return (
+              <ProductCard
+                key={idx}
+                item={item}
+                likeProducts={likeProducts}
+                setLikeProducts={setLikeProducts}
+                className={'main'}
+              />
+            );
+          })}
+        </div>
+        <div className="fetch-button">
+          <Icon icon={IconType.REFRESH} size={2} />
+          <span className="title">{title}</span>
+          <span>다른 상품 보기 </span>
+          <span className="page-count">
+            <span className="current-count">{currentSlide}</span>/
+            <span className="total-count">{totalSlides}</span>
+          </span>
+        </div>
+      </div>
+    </S.FetchableContainer>
+  );
+};
