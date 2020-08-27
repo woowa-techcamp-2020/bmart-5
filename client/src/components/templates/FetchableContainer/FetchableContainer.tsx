fd41cf907a5454b413c8b32a6eb5d9fca584609f
younho9 <younho9.choo@gmail.com>
2020-08-27 17:06:23 +0900

    chore: FetchableContainer 기본 생성 #164

---
+++ client/src/components/templates/FetchableContainer/FetchableContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import * as S from './styled';
+
+type Props = {};
+
+export const FetchableContainer: React.FC<Props> = (props) => {
+  return <S.FetchableContainer />;
+};
+
---
+++ client/src/components/templates/FetchableContainer/styled.tsx
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+
+export const FetchableContainer = styled.div<{}>``
