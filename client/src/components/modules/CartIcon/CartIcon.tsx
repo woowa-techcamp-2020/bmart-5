2e94097da45dafa843e34806cd5d7165641150b5
younho9 <younho9.choo@gmail.com>
2020-08-28 05:20:52 +0900

    feat: CartIcon 추가 #182

---
+++ client/src/components/modules/CartIcon/CartIcon.tsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import * as S from './styled';
+import Icon from '@components/atoms/Icon';
+import { useRouter } from 'next/router';
+import { getCookie } from '@utils/cookie-manager';
+import { Context } from '@commons/Context';
+
+type Props = {};
+
+export const CartIcon: React.FC<Props> = () => {
+  const { cartProducts } = useContext(Context);
+  const router = useRouter();
+  const token = getCookie('authorization');
+  const onCartIconClickHandler = () => {
+    if (!token) {
+      confirm('로그인 하시겠습니까?') ? router.push('/signin') : false;
+      return;
+    }
+    router.push('/cart');
+  };
+
+  return (
+    <S.CartIcon onClick={onCartIconClickHandler}>
+      <Icon icon={'Basket'} size={5}></Icon>
+      <S.Badge>{cartProducts.length}</S.Badge>
+    </S.CartIcon>
+  );
+};
