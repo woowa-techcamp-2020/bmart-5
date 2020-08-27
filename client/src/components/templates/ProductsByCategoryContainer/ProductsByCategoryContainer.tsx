58848a8bcac736f6ef507f2b1c57129ae06bfac3
younho9 <younho9.choo@gmail.com>
2020-08-27 12:59:47 +0900

    feat: main Type의 더보기 버튼 라우팅 기능 추가 #151

--- client/src/components/templates/ProductsByCategoryContainer/ProductsByCategoryContainer.tsx
+++ client/src/components/templates/ProductsByCategoryContainer/ProductsByCategoryContainer.tsx
@@ -3,19 +3,35 @@ import * as S from './styled';
 import ProductCard from '@components/modules/ProductCard';
 import { ProductType } from '@pages/index';
 import ContainerHeader from '@components/modules/ContainerHeader';
+import { useRouter } from 'next/router';
 
 type ProductArrType = Array<ProductType>;
 
 type Props = {
+  categoryId?: number;
   name?: string;
   products: ProductArrType;
   headerType: 'main' | 'filter';
 };
 
-export const ProductsByCategoryContainer: React.FC<Props> = ({ name, products, headerType }) => {
+export const ProductsByCategoryContainer: React.FC<Props> = ({
+  categoryId,
+  name,
+  products,
+  headerType,
+}) => {
+  const router = useRouter();
+
   return (
     <S.ProductsByCategoryContainer>
-      {headerType === 'main' && <ContainerHeader moreBtn>{name}</ContainerHeader>}
+      {headerType === 'main' && (
+        <ContainerHeader
+          moreBtn
+          onMoreBtnClickHandler={() => router.replace(`/categories/${categoryId}`)}
+        >
+          {name}
+        </ContainerHeader>
+      )}
       {headerType === 'filter' && (
         <ContainerHeader>
           <div />
