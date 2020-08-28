9db3a6f04bd75a31cc073302bebdede0e783fd89
MyeongSeong <maeng2418@naver.com>
2020-08-28 12:07:39 +0900

    chore: 헤더 밑에 그림자

--- client/src/components/modules/Header/styled.tsx
+++ client/src/components/modules/Header/styled.tsx
@@ -16,6 +16,8 @@ export const Header = styled.header`
   align-items: center;
   padding: 2rem 0;
   background-color: white;
+  color: rgba(0, 0, 0, 0.9);
+  box-shadow: 0px 2px 10px;
 
   & > div.left-wrap {
     display: flex;
